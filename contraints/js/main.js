8114e91e323b2bc6822cfe011d7c49073441436f
bvchuong <buichuong@hotmail.com>
2017-08-30 17:04:40 +0700

    update explore soure code

--- contraints/js/main.js
+++ contraints/js/main.js
@@ -1,17 +1,69 @@
-var contraints = {
-    video: true
-};
-
+var vgaButton = document.querySelector('button#vga');
+var qvgaButton = document.querySelector('button#qvga');
+var hdButton = document.querySelector('button#hd');
 var video = document.querySelector('video');
 
-function handleSuccess(stream){
+navigator.getUserMedia = navigator.getUserMedia||navigator.webkitGetUserMedia||navigator.mozGetUserMedia;
+
+function successCallback(stream){
     window.stream = stream;
     video.src = window.URL.createObjectURL(stream);
 }
 
-function handleError(error){
+function errorCallback(err){
     alert("Có lỗi xảy ra");
-    console.log('navigator.getUserMedia error: ',error);
+    console.log('navigator.getUserMedia error: ', err);
 }
 
-navigator.mediaDevices.getUserMedia(contraints).then(handleSuccess).catch(handleError);
+function displayVideoDimensions(){
+    dimensions.textContent ='Kích thước của video: ' + video.videoWidth + 'x' + video.videoHeight + 'px';
+}
+
+video.addEventListener('play',function(){
+    setTimeout(function() {
+        displayVideoDimensions();
+    }, 500);
+})
+
+var qvgaConstraints = {
+    video: {
+      mandatory: {
+        maxWidth: 320,
+        maxHeight: 180
+      }
+    }
+  };
+  var vgaConstraints = {
+    video: {
+      mandatory: {
+        maxWidth: 640,
+        maxHeight: 360
+      }
+    }
+  };
+  var hdConstraints = {
+    video: {
+      mandatory: {
+        minWidth: 1280,
+        minHeight: 720
+      }
+    }
+  };
+
+  function getMedia(constraints){
+      if(window.stream){
+          video.src = null;
+          window.stream.getVideoTracks()[0].stop();
+      }
+      navigator.getUserMedia(constraints,successCallback,errorCallback);
+  }
+
+  qvgaButton.onclick = function(){
+      getMedia(qvgaConstraints);
+  }
+  vgaButton.onclick = function(){
+      getMedia(vgaConstraints);
+  }
+  hdButton.onclick = function(){
+    getMedia(hdConstraints);
+  }
